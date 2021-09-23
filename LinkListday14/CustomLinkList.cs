39f7a0de6b167afda3fd4a7fddab97602d3d8fe7
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-23 08:51:48 +0530

    [ShubhamPV] Linked list UC1

---
+++ LinkListday14/CustomLinkList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListday14
+{
+    class CustomLinkList
+    {
+        public Node head;
+        ///</summary>
+        ///UC1 insert at last
+        ///</summary>
+        ///<param  name= "New data" ></param>
+        public void InsertLast(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = new_node;
+            }
+            Console.WriteLine("inserted into list:" + new_node.data);
+        }
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
---
+++ LinkListday14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListday14
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CustomLinkList customLinkList = new CustomLinkList();
+            //UC1 insertLast
+            customLinkList.InsertLast(56);
+            customLinkList.InsertLast(30);
+            customLinkList.InsertLast(70);
+            Console.ReadLine();
+        }
+    }
+}
